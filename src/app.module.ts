3a1122cf371c99a13d141a5ede917d122edfda04
JuanMAA <42298951+JuanMAA@users.noreply.github.com>
2023-09-23 12:11:50 -0300

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesController } from './note/note.controller';
 import { Note } from './note/note.entity';
 import { NotesService } from './note/note.service';
+import * as cors from 'cors';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Note]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST, 
+      host: process.env.DATABASE_HOST,
       port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER, 
-      password: process.env.DATABASE_PASSWORD, 
-      database: process.env.DATABASE_NAME, 
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
-      synchronize: eval(process.env.DATABASE_SYNCHRONIZE), 
+      synchronize: eval(process.env.DATABASE_SYNCHRONIZE),
     })
   ],
   controllers: [NotesController],
   providers: [NotesService],
 })
-export class AppModule { }
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    const whitelist = ['https://localhost.30000', process.env.ORIGIN_CORS];
+    const corsOptions = {
+      origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Acceso no permitido por CORS'));
+        }
+      },
+    };
+    consumer.apply(cors(corsOptions)).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.ORIGIN_CORS,
+    origin: true,
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Accept-Encoding, Accept-Language, Host, Origin, User-Agent',
     methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
     credentials: true,
